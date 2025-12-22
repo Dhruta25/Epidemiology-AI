7b435d0f6b4c307df6e317623e026592d7e19583
saloni259 <salonisahoo25@gmail.com>
2025-12-22 22:07:45 +0530

    cloudinary connection

---
+++ REbackend/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+import dotenv from "dotenv"
+dotenv.config({path:"./.env"})
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if(!localfilepath) return null;
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type: "auto"
+        })
+        console.log("file is upload on cloudinary",response.url);
+        fs.unlink(localfilepath);
+        return response;
+    } catch (error) {
+        console.log("Error find during upload on cloudinary",error?.message)
+        fs.unlink(localfilepath);
+        return null;
+    }
+
+   
+}
+
+ export {uploadOnCloudinary}
