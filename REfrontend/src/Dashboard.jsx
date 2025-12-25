0b208ca3a068d2d9243712e9ab44ba3480b10e9b
saloni259 <salonisahoo25@gmail.com>
2025-12-26 00:00:57 +0530

    Wellcome page design

--- REfrontend/src/Dashboard.jsx
+++ REfrontend/src/Dashboard.jsx
@@ -1,19 +1,33 @@
-import { Link } from "react-router-dom"
-import "./Dashboard.css"
-function Dashboard(){
-     return(
-        <div className="container">
-          <h1 className="heading">WELLCOME !!!!!</h1>
-          <div className="buttons">
-             <div className="login_button">
-                <Link to="/login" className="login">login</Link>
-              </div>
-          
-             <div className="register_button">
-               <Link to="/register" className="register">Register</Link>
-             </div>
+import { Link } from "react-router-dom";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="video-container">
+      
+      <video className="bg-video" autoPlay loop muted>
+        <source src="public/bg1.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+
+      <div className="content">
+        <div>
+        <h1 className="heading">WELCOME TO EPIDEMIOLOGY-AI</h1>
+        <h2>We help you understand and track disease trends using the power of artificial intelligence.
+
+Our goal is to make health information easier to access, easier to understand, and useful for keeping communities safe.</h2>
+        </div>
+        <div className="buttons">
+          <div className="login_button">
+            <Link to="/login" className="login">Login</Link>
+          </div>
+          <div className="register_button">
+            <Link to="/register" className="register">Register</Link>
           </div>
         </div>
-     )
+      </div>
+    </div>
+  );
 }
-export default Dashboard
+
+export default Dashboard;
